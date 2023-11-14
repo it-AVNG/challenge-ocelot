b9f135df83cdf18d194bbc654d38171079de5e9f
it-AVNG <it.avng@gmail.com>
2023-11-14 20:17:56 +0700

    fix syntax to pass flake8

--- app/core/models.py
+++ app/core/models.py
@@ -22,10 +22,11 @@ class UserManager(BaseUserManager):
 
         return user
 
-class User(AbstractBaseUser,PermissionsMixin):
+
+class User(AbstractBaseUser, PermissionsMixin):
     '''user in system'''
 
-    email=models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
