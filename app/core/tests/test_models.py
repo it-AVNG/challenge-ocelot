c937b11f98187621e66b5c9e24389cf97b4ab114
it-AVNG <it.avng@gmail.com>
2023-11-15 11:48:03 +0700

    add feature to raise error when user email is invalid

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,3 +33,9 @@ class ModelTests(TestCase):
         for email, expected in sample_email:
             user = get_user_model().objects.create_user(email,'sample123')
             self.assertEqual(user.email,expected)
+
+    def test_new_user_without_email_raises_error(self):
+        '''Test that creating user without an email raise ValueError'''
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
