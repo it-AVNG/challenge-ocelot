9da63a6c0de7fa9a177eae7de65ba511bc9a6af1
it-AVNG <it.avng@gmail.com>
2023-11-15 13:28:50 +0700

    add customized page for Users in admin app

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
+'''
+Django admin customize
+'''
 from django.contrib import admin  # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    '''define Admin pages for users'''
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
