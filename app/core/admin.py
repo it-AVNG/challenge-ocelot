6e336d248837ebf9fb31a4750fdecaf5c8d2d12a
it-AVNG <it.avng@gmail.com>
2023-11-15 13:44:32 +0700

    add user edit page feature to admin app

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,9 @@ Django admin customize
 '''
 from django.contrib import admin  # noqa
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# future proffing for translation
+from django.utils.translation import gettext_lazy as _
+
 from core import models
 
 
@@ -11,6 +14,27 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permission'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (
+            _('Important dates'), {
+                'fields': (
+                    'last_login',
+                )
+            }
+        )
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
