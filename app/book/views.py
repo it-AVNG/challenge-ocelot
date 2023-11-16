f998f42231e3ba4df3f9835401f19358893bec98
it-AVNG <it.avng@gmail.com>
2023-11-16 16:19:23 +0700

    create BookViewSet

--- app/book/views.py
+++ app/book/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+'''
+Views for the Book API
+'''
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from core.models import Book
+from book import serializers
+
+class BookViewSet(viewsets.ModelViewSet):
+    '''View for manage book APIs'''
+
+    serializer_class = serializers.BookSerializer
+    queryset = Book.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        '''retrive books'''
+
+        return self.queryset.order_by('-id')
