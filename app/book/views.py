da6612c4b2ef81179008974eea6ce78124a7c547
it-AVNG <it.avng@gmail.com>
2023-11-16 19:43:18 +0700

    change BookViewSet default serializer to BookDetailSerializer, only show Bookserializer when action is 'list'

--- app/book/views.py
+++ app/book/views.py
@@ -12,7 +12,7 @@ from book import serializers
 class BookViewSet(viewsets.ModelViewSet):
     '''View for manage book APIs'''
 
-    serializer_class = serializers.BookSerializer
+    serializer_class = serializers.BookDetailSerializer
     queryset = Book.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -21,3 +21,11 @@ class BookViewSet(viewsets.ModelViewSet):
         '''retrive books'''
 
         return self.queryset.order_by('-id')
+
+    def get_serializer_class(self):
+        '''return the serializer class for request'''
+
+        if self.action == 'list':
+            return serializers.BookSerializer
+
+        return self.serializer_class
