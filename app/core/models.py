23701453b4c78dfd04a6e9c3e47252a37a11745b
it-AVNG <it.avng@gmail.com>
2023-11-16 10:39:56 +0700

    add Book models

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 '''
 Database Models
 '''
-
+from django.conf import settings
 from django.db import models  # noqa
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -48,3 +48,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Book(models.Model):
+    '''Book objects'''
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
