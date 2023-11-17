7434cbab71f4347ae4aa52a725a49d20c2f16b1c
it-AVNG <it.avng@gmail.com>
2023-11-17 18:30:17 +0700

    syntax comply with flake8

--- app/core/views.py
+++ app/core/views.py
@@ -6,6 +6,7 @@ core views health check for app.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(['GET'])
 def health_check(request):
     '''returns successful response.'''
