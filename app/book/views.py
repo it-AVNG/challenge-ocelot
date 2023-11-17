52b878f3d491e73e44a88219e7e3aa0f00e6c8a2
it-AVNG <it.avng@gmail.com>
2023-11-17 11:35:02 +0700

    add new method to handle upload image action for bookview

--- app/book/views.py
+++ app/book/views.py
@@ -1,7 +1,9 @@
 '''
 Views for the Book API
 '''
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
@@ -28,6 +30,8 @@ class BookViewSet(viewsets.ModelViewSet):
 
         if self.action == 'list':
             return serializers.BookSerializer
+        elif self.action == 'upload_image':
+            return serializers.BookImageSerializer
 
         return self.serializer_class
 
@@ -35,3 +39,16 @@ class BookViewSet(viewsets.ModelViewSet):
         '''create new book'''
 
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        '''Upload image to a book'''
+
+        book = self.get_object()
+        serializer = self.get_serializer(book, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
