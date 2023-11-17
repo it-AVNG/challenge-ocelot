baa9ff0c8cee698c00464f5242405ed52c777240
it-AVNG <it.avng@gmail.com>
2023-11-17 09:20:32 +0700

    add throttling for the app

--- app/app/settings.py
+++ app/app/settings.py
@@ -135,5 +135,13 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 
 }
